d16441ce248682cb5602587862596f0d4bbfa649
Chirag <bansalc73@gmail.com>
2020-07-25 22:11:21 +0530

    Some more functionality

--- scripts.js
+++ scripts.js
@@ -12,5 +12,27 @@ $(document).ready(function () {
             $('#carouselButton').children('span').addClass('fa-pause');
         }
     });
-   
 });
+
+
+$(window).on("load resize scroll", function() {
+    $(".bg-static").each(function() {
+      var windowTop = $(window).scrollTop();
+      var elementTop = $(this).offset().top;
+      var leftPosition = windowTop - elementTop;
+      console.log(windowTop);
+      console.log(elementTop);
+        $(this)
+          .find(".bg-move")
+          .css({ left: leftPosition });
+        $(this)
+          .find(".bg-move1")
+          .css({ right: leftPosition });
+    });
+  });
+
+  $(window).scroll(function(){
+    $(".fade-out").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
+  
+  
