27c33ad815dad07d4cdaf9177b09ae8f7ee9a49c
Chirag <bansalc73@gmail.com>
2020-07-26 00:45:51 +0530

    First Three Pages

--- scripts.js
+++ scripts.js
@@ -14,23 +14,6 @@ $(document).ready(function () {
     });
 });
 
-
-$(window).on("load resize scroll", function() {
-    $(".bg-static").each(function() {
-      var windowTop = $(window).scrollTop();
-      var elementTop = $(this).offset().top;
-      var leftPosition = windowTop - elementTop;
-      console.log(windowTop);
-      console.log(elementTop);
-        $(this)
-          .find(".bg-move")
-          .css({ left: leftPosition });
-        $(this)
-          .find(".bg-move1")
-          .css({ right: leftPosition });
-    });
-  });
-
   $(window).scroll(function(){
     $(".fade-out").css("opacity", 1 - $(window).scrollTop() / 250);
   });
